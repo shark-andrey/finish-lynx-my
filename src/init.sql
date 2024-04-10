930ffcf74b88a97ed6fa50d7c34db437963ec508
shark-andrey <163378908+shark-andrey@users.noreply.github.com>
2024-04-10 17:01:32 +0500

    Update init.sql

--- src/init.sql
+++ src/init.sql
@@ -1,1 +1,14 @@
-
+create table if not exists TABLE_NAME (
+    event_number int not null,
+    round_number int not null,
+    heat_number int not null,
+    place varchar(255),
+    athlete_id int not null,
+    lane int,
+    time varchar(255),
+    react_time decimal(7, 2),
+    wind varchar(255),
+    photo_file_name varchar(255),
+    competition_id int not null,
+    primary key (event_number, round_number, heat_number, athlete_id)
+);
