3523250060840109e56feb526c5ab4e871f24b73
shark-andrey <163378908+shark-andrey@users.noreply.github.com>
2024-04-10 16:58:47 +0500

    Delete scr directory

--- scr/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-port = os.getenv("PORT", 7799)
-db_url = os.getenv("DB_URL", "mysql+asyncmy://root:password@localhost:3306/dev")
-# db_url = os.getenv("DB_URL", "mysql+pymysql://root:password@localhost:3306/dev")
-table_name = "finish_lynx"
